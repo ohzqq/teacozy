86e47d120a56fea2f2d16e2e6e7c441347e140a2
ohzqq <iamchurkey@gmail.com>
2022-10-27 14:00:07 -0400

    util

--- frame/frame.go
+++ frame/frame.go
@@ -3,5 +3,14 @@ package frame
 import "github.com/charmbracelet/bubbles/viewport"
 
 type Model struct {
-	model viewport.Model
+	model  viewport.Model
+	Style  Styles
+	Width  int
+	Height int
+}
+
+func New() Model {
+	style := DefaultStyle()
+	model := viewport.New()
+	return Model{}
 }
