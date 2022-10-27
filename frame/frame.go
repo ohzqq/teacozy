378e587cf8ec951de3d51ebccfc86c0f01e9828c
ohzqq <iamchurkey@gmail.com>
2022-10-27 14:08:33 -0400

    keys

--- frame/frame.go
+++ frame/frame.go
@@ -1,16 +1,79 @@
 package frame
 
-import "github.com/charmbracelet/bubbles/viewport"
+import (
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 type Model struct {
 	model  viewport.Model
 	Style  Styles
 	Width  int
 	Height int
+	KeyMap KeyMap
 }
 
-func New() Model {
+func New(w, h int) Model {
 	style := DefaultStyle()
-	model := viewport.New()
-	return Model{}
+	model := viewport.New(w, h)
+	model.Style = style.Render()
+	model.KeyMap = keyMap()
+	return Model{
+		model:  model,
+		Style:  style,
+		Width:  w,
+		Height: h,
+	}
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		cmd  tea.Cmd
+		cmds []tea.Cmd
+	)
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, m.Keys.ExitScreen):
+			cmds = append(cmds, tea.Quit)
+		}
+	}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) View() string {
+	return m.model.View()
+}
+
+func keyMap() viewport.KeyMap {
+	km := viewport.DefaultKeyMap()
+	km.PageDown = key.NewBinding(
+		key.WithKeys("pgdown"),
+		key.WithHelp("pgdn", "page down"),
+	)
+	km.PageUp = key.NewBinding(
+		key.WithKeys("pgup"),
+		key.WithHelp("pgup", "page up"),
+	)
+	km.HalfPageUp = key.NewBinding(
+		key.WithKeys("ctrl+u"),
+		key.WithHelp("ctrl+u", "½ page up"),
+	)
+	km.HalfPageDown = key.NewBinding(
+		key.WithKeys("ctrl+d"),
+		key.WithHelp("ctrl+d", "½ page down"),
+	)
+	km.Up = key.NewBinding(
+		key.WithKeys("up"),
+		key.WithHelp("↑", "up"),
+	)
+	km.Down = key.NewBinding(
+		key.WithKeys("down"),
+		key.WithHelp("↓", "down"),
+	)
+	return km
 }
