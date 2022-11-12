97b8999c6e3d8740385ed338929ccca6e3fe8db7
ohzqq <iamchurkey@gmail.com>
2022-11-12 10:15:53 -0500

    util

--- command.go
+++ command.go
@@ -1,7 +1,11 @@
 package teacozy
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/ohzqq/teacozy/info"
+)
 
+// ui commands
 type SetFocusedViewMsg string
 
 func SetFocusedViewCmd(v string) tea.Cmd {
@@ -25,5 +29,141 @@ type UpdateStatusMsg string
 func UpdateStatusCmd(status string) tea.Cmd {
 	return func() tea.Msg {
 		return UpdateStatusMsg(status)
+	}
+}
+
+// form commands
+type SaveAsHashMsg struct{}
+
+func SaveAsHashCmd() tea.Cmd {
+	return func() tea.Msg {
+		return SaveAsHashMsg{}
+	}
+}
+
+type EditInfoMsg struct{}
+
+func EditInfoCmd() tea.Cmd {
+	return func() tea.Msg {
+		return EditInfoMsg{}
+	}
+}
+
+type EditItemMsg struct {
+	info.Field
+}
+
+func EditItemCmd(field info.Field) tea.Cmd {
+	return func() tea.Msg {
+		return EditItemMsg{Field: field}
+	}
+}
+
+type UpdateContentMsg struct {
+	info.Field
+}
+
+func UpdateContentCmd(key, val string) tea.Cmd {
+	return func() tea.Msg {
+		return UpdateContentMsg{Field: info.NewDefaultField(key, val)}
+	}
+}
+
+// info commands
+type HideMsg struct{}
+
+func HideCmd() tea.Cmd {
+	return func() tea.Msg {
+		return HideMsg{}
+	}
+}
+
+type ShowMsg struct{}
+
+func ShowCmd() tea.Cmd {
+	return func() tea.Msg {
+		return ShowMsg{}
+	}
+}
+
+type EditMsg struct {
+	Fields *Fields
+}
+
+func EditCmd(f *Fields) tea.Cmd {
+	return func() tea.Msg {
+		return EditMsg{Fields: f}
+	}
+}
+
+//item commands
+
+type ToggleListMsg struct{ *Item }
+
+func ToggleListCmd(item *Item) tea.Cmd {
+	return func() tea.Msg {
+		return ToggleListMsg{Item: item}
+	}
+}
+
+type ToggleSelectedMsg struct{ *Item }
+
+func ToggleSelectedCmd(item *Item) tea.Cmd {
+	return func() tea.Msg {
+		return ToggleSelectedMsg{Item: item}
+	}
+}
+
+type ShowInfoMsg struct{ *Item }
+
+func ShowInfoCmd(item *Item) tea.Cmd {
+	return func() tea.Msg {
+		return ShowInfoMsg{Item: item}
+	}
+}
+
+type EditContentMsg struct{ *Item }
+
+func EditContentCmd(item *Item) tea.Cmd {
+	return func() tea.Msg {
+		return EditContentMsg{Item: item}
+	}
+}
+
+// list commands
+
+type ReturnSelectionsMsg struct{}
+
+func ReturnSelectionsCmd() tea.Cmd {
+	return func() tea.Msg {
+		return ReturnSelectionsMsg{}
+	}
+}
+
+func ToggleAllItemsCmd(l *Model) {
+	l.Items.ToggleAllSelectedItems()
+}
+
+type UpdateVisibleItemsMsg string
+
+func UpdateVisibleItemsCmd(opt string) tea.Cmd {
+	return func() tea.Msg {
+		return UpdateVisibleItemsMsg(opt)
+	}
+}
+
+type UpdateStatusMsg struct{ Msg string }
+
+func UpdateStatusCmd(status string) tea.Cmd {
+	return func() tea.Msg {
+		return UpdateStatusMsg{Msg: status}
+	}
+}
+
+type SetSizeMsg []int
+
+func SetSizeCmd(size []int) tea.Cmd {
+	return func() tea.Msg {
+		return SetSizeMsg(size)
 	}
 }
---
+++ util.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"os"
+
+	"github.com/muesli/termenv"
+	"golang.org/x/term"
+)
+
+func ClearScreen() {
+	termenv.ClearScreen()
+}
+
+func TermSize() (int, int) {
+	w, h, _ := term.GetSize(int(os.Stdin.Fd()))
+	return w, h
+}
+
+func TermWidth() int {
+	w, _, _ := term.GetSize(int(os.Stdin.Fd()))
+	return w
+}
+
+func TermHeight() int {
+	_, h, _ := term.GetSize(int(os.Stdin.Fd()))
+	return h
+}
