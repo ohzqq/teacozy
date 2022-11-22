ca33fb4d8206bc7872932e60a28c28f37e454cfe
ohzqq <iamchurkey@gmail.com>
2022-11-22 15:50:34 -0500

    info working

--- info/info.go
+++ info/info.go
@@ -6,31 +6,58 @@ import (
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/ohzqq/teacozy"
 	"github.com/ohzqq/teacozy/keybind"
 	"github.com/ohzqq/teacozy/style"
 )
 
 type Info struct {
-	Model     viewport.Model
-	HideKeys  bool
-	IsVisible bool
-	Editable  bool
-	content   []string
-	Frame     style.Frame
-	Data      teacozy.FormData
-	Style     style.Field
+	Model    viewport.Model
+	HideKeys bool
+	visible  bool
+	Editable bool
+	content  []string
+	title    string
+	Frame    style.Frame
+	Data     teacozy.FormData
+	Style    Style
+}
+
+type Style struct {
+	style.Field
+	Title lipgloss.Style
 }
 
 func New(data teacozy.FormData) *Info {
+	s := Style{
+		Field: style.DefaultFieldStyles(),
+		Title: lipgloss.NewStyle().Foreground(style.Color.Pink),
+	}
 	return &Info{
 		Data:  data,
-		Style: style.DefaultFieldStyles(),
+		Style: s,
 	}
+}
+
+func (i *Info) SetData(data teacozy.FormData) *Info {
+	i.Data = data
+	return i
+}
+
+func (i *Info) SetTitle(title string) *Info {
+	i.title = title
+	return i
 }
 
 func (i Info) RenderData() string {
 	var info []string
+
+	if i.title != "" {
+		t := i.Style.Title.Render(i.title)
+		info = append(info, t)
+	}
+
 	for _, key := range i.Data.Keys() {
 		fd := i.Data.Get(key)
 
@@ -71,6 +98,22 @@ func (i *Info) AddContent(content ...string) *Info {
 	return i
 }
 
+func (i *Info) Show() {
+	i.visible = true
+}
+
+func (i *Info) Hide() {
+	i.visible = false
+}
+
+func (i *Info) Toggle() {
+	i.visible = !i.visible
+}
+
+func (i Info) Visible() bool {
+	return i.visible
+}
+
 //func (m *Info) Update(msg tea.Msg) (*Info, tea.Cmd) {
 func (m *Info) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var (
@@ -84,10 +127,12 @@ func (m *Info) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			cmds = append(cmds, tea.Quit)
 		}
 		switch {
-		case key.Matches(msg, keybind.SaveAndExit):
+		case key.Matches(msg, keybind.EditField):
 			if m.Editable {
 				cmds = append(cmds, EditInfoCmd())
 			}
+		case key.Matches(msg, keybind.InfoKey):
+			m.Toggle()
 		case key.Matches(msg, keybind.HelpKey):
 			fallthrough
 		case key.Matches(msg, keybind.ExitScreen):
@@ -97,6 +142,8 @@ func (m *Info) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	case tea.WindowSizeMsg:
 		m.Model = viewport.New(msg.Width-2, msg.Height-2)
+	case HideInfoMsg:
+		m.Hide()
 	}
 
 	m.Model, cmd = m.Model.Update(msg)
@@ -106,14 +153,18 @@ func (m *Info) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m *Info) Init() tea.Cmd {
+	m.Show()
 	return nil
 }
 
 func (m *Info) View() string {
-	content := m.RenderData()
-	if c := m.content; len(m.content) > 0 {
-		content = strings.Join(c, "\n")
+	if m.Visible() {
+		content := m.RenderData()
+		if c := m.content; len(m.content) > 0 {
+			content = strings.Join(c, "\n")
+		}
+		m.Model.SetContent(content)
+		return m.Model.View()
 	}
-	m.Model.SetContent(content)
-	return m.Model.View()
+	return ""
 }
