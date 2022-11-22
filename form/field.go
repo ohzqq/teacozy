bf09f72d68866c97e6e34113e9a6274d0ccef427
ohzqq <iamchurkey@gmail.com>
2022-11-22 12:13:58 -0500

    delegate

--- form/field.go
+++ form/field.go
@@ -1,1 +1,10 @@
 package form
+
+type Fields struct {
+	fields []*Field
+}
+
+type Field struct {
+	key   string
+	value string
+}
--- form/form.go
+++ form/form.go
@@ -1,1 +1,12 @@
 package form
+
+import (
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/ohzqq/teacozy/list"
+)
+
+type Form struct {
+	*Fields
+	Model list.Model
+	Input textarea.Model
+}
