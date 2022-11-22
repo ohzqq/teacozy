2687c95ea2723c05358da3a854b1b5fcebe28776
ohzqq <iamchurkey@gmail.com>
2022-11-22 14:23:09 -0500

    frame style works

--- util/util.go
+++ util/util.go
@@ -25,3 +25,35 @@ func TermHeight() int {
 	_, h, _ := term.GetSize(int(os.Stdin.Fd()))
 	return h
 }
+
+func CalculateWidth(min, width int) int {
+	max := TermWidth()
+	w := min
+
+	if width != 0 {
+		switch {
+		case width < min:
+			w = width
+		case width > max:
+			w = max
+		}
+	}
+
+	return w
+}
+
+func CalculateHeight(min, height int) int {
+	max := TermHeight()
+	h := min
+
+	if height != 0 {
+		switch {
+		case height < min:
+			h = height
+		case height > max:
+			h = max
+		}
+	}
+
+	return h
+}
