921790026a4d0cb0e2d2aad18060b838f1437753
ohzqq <iamchurkey@gmail.com>
2022-11-26 11:57:57 -0500

    Basic item works

--- list/item.go
+++ list/item.go
@@ -1,1 +1,156 @@
 package list
+
+import (
+	"github.com/ohzqq/teacozy"
+	"github.com/ohzqq/teacozy/form"
+)
+
+type Item struct {
+	idx          int
+	IsHidden     bool
+	IsSelected   bool
+	MultiSelect  bool
+	ShowChildren bool
+	ListLevels   int
+	Level        int
+	Parent       *Item
+	//Children     Items
+	hasFields bool
+	Meta      *form.Fields
+	teacozy.Field
+}
+
+func NewItem(item teacozy.Field) *Item {
+	return &Item{
+		Field: item,
+		Meta:  form.NewFields(),
+	}
+}
+
+func (i *Item) SetMeta(meta teacozy.Fields) {
+	i.Meta.SetData(meta)
+}
+
+func (i Item) HasMeta() bool {
+	return len(i.Meta.Keys()) > 0
+}
+
+// Satisfy Fields interface
+func (i Item) Get(key string) teacozy.Field {
+	return i.Meta.Get(key)
+}
+
+func (i Item) Keys() []string {
+	return i.Meta.Keys()
+}
+
+// Satisfy list.Item interface
+func (i Item) FilterValue() string {
+	return i.Content()
+}
+
+// Item methods
+func (i Item) Index() int {
+	return i.idx
+}
+
+func (i *Item) SetMultiSelect() *Item {
+	i.MultiSelect = true
+	return i
+}
+
+func (i *Item) ToggleSelected() *Item {
+	i.IsSelected = !i.IsSelected
+	return i
+}
+
+func (i *Item) Select() *Item {
+	i.IsSelected = true
+	return i
+}
+
+func (i *Item) Deselect() *Item {
+	i.IsSelected = false
+	return i
+}
+
+func (i *Item) Show() *Item {
+	i.IsHidden = false
+	return i
+}
+
+func (i *Item) Hide() *Item {
+	i.IsHidden = true
+	return i
+}
+
+// Sublist methods
+//func (i *Item) Flatten() []*Item {
+//  var items []*Item
+//  depth := 0
+//  if i.HasChildren() {
+//    depth++
+//    for _, item := range i.Children.flat {
+//      if i.MultiSelect {
+//        item.SetMultiSelect()
+//      }
+//      item.IsHidden = true
+//      items = append(items, item)
+//      if item.HasChildren() {
+//        depth++
+//        items = append(items, item.Flatten()...)
+//      }
+//    }
+//  }
+//  i.ListLevels = depth
+//  return items
+//}
+
+//func (i Item) ListDepth() int {
+//  depth := 0
+//  if i.HasChildren() {
+//    depth++
+//    for _, item := range i.Children.flat {
+//      if item.HasChildren() {
+//        depth++
+//      }
+//    }
+//  }
+//  return depth
+//}
+
+//func (i Item) HasChildren() bool {
+//  has := len(i.Children.flat) > 0
+//  return has
+//}
+
+//func (i Item) TotalChildren() int {
+//  if i.HasChildren() {
+//    return len(i.Children.flat)
+//  }
+//  return 0
+//}
+
+func (i *Item) ToggleList() *Item {
+	i.ShowChildren = !i.ShowChildren
+	return i
+}
+
+func (i *Item) Open() *Item {
+	i.ShowChildren = true
+	return i
+}
+
+func (i *Item) Close() *Item {
+	i.ShowChildren = false
+	return i
+}
+
+func (i *Item) SetLevel(l int) *Item {
+	i.Level = l
+	return i
+}
+
+func (i *Item) IsSub() bool {
+	return i.Level != 0
+}
--- list/list.go
+++ list/list.go
@@ -1,40 +1,34 @@
 package list
 
-import (
-	"github.com/charmbracelet/bubbles/list"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/ohzqq/teacozy/style"
-)
+//type ActionFunc func(items ...*Item) tea.Cmd
 
-type ActionFunc func(items ...*Item) tea.Cmd
+//type List struct {
+//  Model         list.Model
+//  Title         string
+//  SelectionList bool
+//  ActionFunc    ActionFunc
+//  Hash          map[string]string
+//  Style         list.Styles
+//  id            int
+//  style.Frame
+//  *Items
+//}
 
-type List struct {
-	Model         list.Model
-	Title         string
-	SelectionList bool
-	ActionFunc    ActionFunc
-	Hash          map[string]string
-	Style         list.Styles
-	id            int
-	style.Frame
-	*Items
-}
+//func NewList() *List {
+//  m := List{
+//    Frame: style.DefaultFrameStyle(),
+//    Items: NewItems(),
+//  }
+//  m.SetAction(PrintItems)
+//  m.Frame.MinHeight = 10
+//  return &m
+//}
 
-func NewList() *List {
-	m := List{
-		Frame: style.DefaultFrameStyle(),
-		Items: NewItems(),
-	}
-	m.SetAction(PrintItems)
-	m.Frame.MinHeight = 10
-	return &m
-}
-
-func NewListModel(w, h int, items *Items) list.Model {
-	l := list.New(items.Visible(), items, w, h)
-	l.SetShowStatusBar(false)
-	l.SetShowHelp(false)
-	l.KeyMap = ListKeyMap()
-	l.Styles = ListStyles()
-	return l
-}
+//func NewListModel(w, h int, items *Items) list.Model {
+//  l := list.New(items.Visible(), items, w, h)
+//  l.SetShowStatusBar(false)
+//  l.SetShowHelp(false)
+//  l.KeyMap = ListKeyMap()
+//  l.Styles = ListStyles()
+//  return l
+//}
