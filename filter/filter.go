b5abca69d60541bd66bc5bcd9ed0a7052db2e2d7
ohzqq <iamchurkey@gmail.com>
2023-04-04 09:43:43 -0400

    roll bakc

--- filter/filter.go
+++ filter/filter.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/paginator"
 	"github.com/charmbracelet/bubbles/textinput"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
@@ -26,7 +25,6 @@ type Filter struct {
 	Matches     []props.Item
 	Input       textinput.Model
 	Viewport    *viewport.Model
-	Paginator   paginator.Model
 	quitting    bool
 	Placeholder string
 	Prompt      string
@@ -78,21 +76,9 @@ func (m *Filter) KeyMap() keys.KeyMap {
 func (m *Filter) Update(msg tea.Msg) tea.Cmd {
 	var cmd tea.Cmd
 	var cmds []tea.Cmd
-	start, end := m.Paginator.GetSliceBounds(len(m.Matches))
 	switch msg := msg.(type) {
 	case message.UpMsg:
-		m.Cursor = util.Clamp(start, end-1, m.Cursor-1)
-
-		//m.Cursor--
-		//if m.Cursor < 0 {
-		//m.Cursor = len(m.Matches) - 1
-		//m.Cursor = 0
-		//m.Paginator.Page = m.Paginator.TotalPages - 1
-		//}
-		//if m.Cursor < start {
-		//m.Cursor = end
-		//m.Paginator.PrevPage()
-		//}
+		m.Cursor = util.Clamp(0, len(m.Matches)-1, m.Cursor-1)
 
 		h := m.Matches[m.Cursor].LineHeight()
 		offset := m.Viewport.YOffset
@@ -105,9 +91,11 @@ func (m *Filter) Update(msg tea.Msg) tea.Cmd {
 			m.Viewport.SetYOffset(offset - h)
 		}
 
-		m.Props().SetCurrent(m.Cursor % m.Props().Height)
+		m.Props().SetCurrent(m.Cursor)
 
 	case message.DownMsg:
+		m.Cursor = util.Clamp(0, len(m.Matches)-1, m.Cursor+1)
+
 		offset := m.Viewport.YOffset
 		h := m.Matches[m.Cursor].LineHeight()
 		if o := h - m.Viewport.Height; o > 0 {
@@ -117,19 +105,8 @@ func (m *Filter) Update(msg tea.Msg) tea.Cmd {
 			m.Viewport.LineDown(h)
 			m.Viewport.SetYOffset(offset + h)
 		}
-		m.Cursor = util.Clamp(start, end-1, m.Cursor+1)
 
-		//m.Cursor++
-		//if m.Cursor >= len(m.Matches) {
-		//m.Cursor = end - 1
-		//m.Paginator.Page = 0
-		//m.Viewport.GotoTop()
-		//}
-		//if m.Cursor >= end {
-		//m.Cursor = end - 1
-		//m.Paginator.NextPage()
-		//}
-		m.Props().SetCurrent(m.Cursor % m.Props().Height)
+		m.Props().SetCurrent(m.Cursor)
 
 	case message.ToggleItemMsg:
 		if len(m.Matches) == 0 {
@@ -170,13 +147,11 @@ func (m *Filter) Update(msg tea.Msg) tea.Cmd {
 		}
 		m.Input, cmd = m.Input.Update(msg)
 		m.Matches = m.Props().Visible(m.Input.Value())
-		m.Paginator.SetTotalPages((len(m.Matches) + m.Props().Height - 1) / m.Props().Height)
-		m.Paginator.PerPage = m.Props().Height
 		cmds = append(cmds, cmd)
 	}
 
-	//m.Cursor = util.Clamp(0, len(m.Matches)-1, m.Cursor)
-	//m.Props().SetCurrent(m.Cursor)
+	m.Cursor = util.Clamp(0, len(m.Matches)-1, m.Cursor)
+	m.Props().SetCurrent(m.Cursor)
 	return tea.Batch(cmds...)
 }
 
@@ -186,14 +161,8 @@ func (m *Filter) Render(w, h int) string {
 
 	var s strings.Builder
 
-	start, end := m.Paginator.GetSliceBounds(len(m.Matches))
+	items := m.Props().RenderItems(m.Cursor, m.Matches)
 
-	//items := m.Props().RenderItems(m.Cursor, m.Matches)
-
-	items := m.Props().RenderItems(
-		m.Cursor%m.Props().Height,
-		m.Matches[start:end],
-	)
 	s.WriteString(items)
 
 	m.Viewport.SetContent(s.String())
@@ -257,11 +226,6 @@ func (tm *Filter) Init(props Props) tea.Cmd {
 	tm.Input.Placeholder = tm.Placeholder
 	tm.Input.Width = tm.Props().Width
 
-	tm.Paginator = paginator.New()
-	tm.Paginator.Type = paginator.Arabic
-	tm.Paginator.SetTotalPages((len(tm.Props().Visible()) + props.Height - 1) / props.Height)
-	tm.Paginator.PerPage = props.Height
-
 	v := viewport.New(0, 0)
 	tm.Viewport = &v
 	tm.Input.Focus()
@@ -271,22 +235,4 @@ func (tm *Filter) Init(props Props) tea.Cmd {
 
 func (m *Filter) ReturnSelections() tea.Cmd {
 	return message.ReturnSels(m.Props().Limit, m.Props().NumSelected)
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-
-	return b
-}
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-
-	return b
-}
-func clamp(v, low, high int) int {
-	return min(max(v, low), high)
 }
