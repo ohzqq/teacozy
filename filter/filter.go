ee6c2ee2ee68bd982108d634c72bef968c3f1fc5
ohzqq <iamchurkey@gmail.com>
2023-04-09 17:07:12 -0400

    table

--- filter/filter.go
+++ filter/filter.go
@@ -11,7 +11,6 @@ import (
 	"github.com/ohzqq/teacozy/message"
 	"github.com/ohzqq/teacozy/props"
 	"github.com/ohzqq/teacozy/style"
-	"github.com/ohzqq/teacozy/util"
 )
 
 type Filter struct {
@@ -22,6 +21,8 @@ type Filter struct {
 	Matches     []props.Item
 	Input       textinput.Model
 	Viewport    *viewport.Model
+	end         int
+	start       int
 	quitting    bool
 	Placeholder string
 	Prompt      string
@@ -72,28 +73,52 @@ func (m *Filter) Update(msg tea.Msg) tea.Cmd {
 	var cmds []tea.Cmd
 	switch msg := msg.(type) {
 	case message.UpMsg:
-		if len(m.Matches) > 0 {
-			m.Cursor = util.Clamp(0, len(m.Matches)-1, m.Cursor-1)
-			m.SetCurrent(m.Cursor)
-			h := m.Matches[m.Cursor].LineHeight()
-			if m.Cursor < m.Viewport.YOffset {
-				m.Viewport.LineUp(h)
-			}
+		n := 1
+		m.Cursor = clamp(m.Cursor-n, 0, len(m.Props().Visible())-1)
+		switch {
+		case m.start == 0:
+			m.Viewport.SetYOffset(clamp(m.Viewport.YOffset, 0, m.Cursor))
+		case m.start < m.Viewport.Height:
+			m.Viewport.SetYOffset(clamp(m.Viewport.YOffset+n, 0, m.Cursor))
+		case m.Viewport.YOffset >= 1:
+			m.Viewport.YOffset = clamp(m.Viewport.YOffset+n, 1, m.Viewport.Height)
 		}
+		m.UpdateViewport()
+		//if len(m.Matches) > 0 {
+		//  m.Cursor = util.Clamp(0, len(m.Matches)-1, m.Cursor-1)
+		//  m.SetCurrent(m.Cursor)
+		//  h := m.Matches[m.Cursor].LineHeight()
+		//  if m.Cursor < m.Viewport.YOffset {
+		//    m.Viewport.LineUp(h)
+		//  }
+		//}
 
 	case message.DownMsg:
-		if len(m.Matches) > 0 {
-			m.Cursor = util.Clamp(0, len(m.Matches)-1, m.Cursor+1)
-			m.SetCurrent(m.Cursor)
+		n := 1
+		m.Cursor = clamp(m.Cursor+n, 0, len(m.Props().Visible())-1)
+		m.UpdateViewport()
 
-			offset := m.Viewport.YOffset
-			h := m.Matches[m.Cursor].LineHeight()
-			if o := h - m.Viewport.Height; o > 0 {
-				m.Viewport.LineDown(o)
-			} else if m.Cursor <= offset+m.Viewport.Height {
-				m.Viewport.LineDown(h)
-			}
+		switch {
+		case m.end == len(m.Props().Visible()):
+			m.Viewport.SetYOffset(clamp(m.Viewport.YOffset-n, 1, m.Viewport.Height))
+		case m.Cursor > (m.end-m.start)/2:
+			m.Viewport.SetYOffset(clamp(m.Viewport.YOffset-n, 1, m.Cursor))
+		case m.Viewport.YOffset > 1:
+		case m.Cursor > m.Viewport.YOffset+m.Viewport.Height-1:
+			m.Viewport.SetYOffset(clamp(m.Viewport.YOffset+1, 0, 1))
 		}
+		//if len(m.Matches) > 0 {
+		//  m.Cursor = util.Clamp(0, len(m.Matches)-1, m.Cursor+1)
+		//  m.SetCurrent(m.Cursor)
+
+		//  offset := m.Viewport.YOffset
+		//  h := m.Matches[m.Cursor].LineHeight()
+		//  if o := h - m.Viewport.Height; o > 0 {
+		//    m.Viewport.LineDown(o)
+		//  } else if m.Cursor <= offset+m.Viewport.Height {
+		//    m.Viewport.LineDown(h)
+		//  }
+		//}
 
 	case message.ToggleItemMsg:
 		if len(m.Matches) > 0 {
@@ -129,15 +154,17 @@ func (m *Filter) Update(msg tea.Msg) tea.Cmd {
 		}
 		m.Input, cmd = m.Input.Update(msg)
 		if v := m.Input.Value(); v != "" {
+			//m.Matches = m.Props().Visible(m.Input.Value())
 			m.Matches = m.Props().Visible(v)
 		} else {
-			m.Matches = []props.Item{}
+			//m.Matches = []props.Item{}
+			m.Matches = m.Props().Visible()
 		}
 		cmds = append(cmds, message.Top())
 		cmds = append(cmds, cmd)
 	}
 
-	m.Cursor = util.Clamp(0, len(m.Matches)-1, m.Cursor)
+	//m.Cursor = util.Clamp(0, len(m.Matches)-1, m.Cursor)
 	return tea.Batch(cmds...)
 }
 
@@ -145,10 +172,34 @@ func (m *Filter) SetCurrent(idx int) {
 	m.Props().SetCurrent(idx)
 }
 
+// UpdateViewport updates the list content based on the previously defined
+// columns and rows.
+func (m *Filter) UpdateViewport() {
+	//renderedRows := make([]string, 0, len(m.Matches))
+	var renderedRows string
+
+	// Render only rows from: m.cursor-m.viewport.Height to: m.cursor+m.viewport.Height
+	// Constant runtime, independent of number of rows in a table.
+	// Limits the number of renderedRows to a maximum of 2*m.viewport.Height
+	if m.Cursor >= 0 {
+		m.start = clamp(m.Cursor-m.Viewport.Height, 0, m.Cursor)
+	} else {
+		m.start = 0
+	}
+	m.end = clamp(m.Cursor+m.Viewport.Height, m.Cursor, len(m.Props().Visible()))
+	//for i := m.start; i < m.end; i++ {
+	renderedRows = m.Props().RenderItems(m.Matches[m.start:m.end])
+	//renderedRows = append(renderedRows, strconv.Itoa(i))
+	//}
+
+	m.Viewport.SetContent(renderedRows)
+}
+
 func (m *Filter) Render(w, h int) string {
 	m.Viewport.Height = m.Props().Height
 	m.Viewport.Width = m.Props().Width
-	m.Viewport.SetContent(m.Props().RenderItems(m.Matches))
+	m.UpdateViewport()
+	//m.Viewport.SetContent(m.Props().RenderItems(m.Matches))
 
 	view := m.Input.View() + "\n" + m.Viewport.View()
 	return view
@@ -165,6 +216,9 @@ func (tm *Filter) Init(props Props) tea.Cmd {
 
 	v := viewport.New(0, 0)
 	tm.Viewport = &v
+	tm.Matches = props.Visible()
+	tm.UpdateViewport()
+
 	tm.Input.Focus()
 
 	return nil
@@ -188,4 +242,24 @@ func StopFiltering() tea.Cmd {
 	return func() tea.Msg {
 		return StopFilteringMsg{}
 	}
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+func clamp(v, low, high int) int {
+	return min(max(v, low), high)
 }
--- list/list.go
+++ list/list.go
@@ -1,10 +1,13 @@
 package list
 
 import (
+	"strconv"
+
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/paginator"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/ohzqq/teacozy/keys"
 	"github.com/ohzqq/teacozy/message"
 	"github.com/ohzqq/teacozy/props"
@@ -19,6 +22,8 @@ type List struct {
 	quitting  bool
 	Style     style.List
 	items     *props.Items
+	end       int
+	start     int
 	Footer    string
 }
 
@@ -138,6 +143,61 @@ func (m *List) SetCurrent() {
 	m.Props().SetCurrent(m.Cursor % m.Props().Height)
 }
 
+// MoveUp moves the selection up by any number of rows.
+// It can not go above the first row.
+func (m *List) MoveUp(n int) {
+	m.Cursor = clamp(m.Cursor-n, 0, len(m.Props().Visible())-1)
+	switch {
+	case m.start == 0:
+		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset, 0, m.Cursor))
+	case m.start < m.Viewport.Height:
+		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset+n, 0, m.Cursor))
+	case m.Viewport.YOffset >= 1:
+		m.Viewport.YOffset = clamp(m.Viewport.YOffset+n, 1, m.Viewport.Height)
+	}
+	m.UpdateViewport()
+}
+
+// MoveDown moves the selection down by any number of rows.
+// It can not go below the last row.
+func (m *List) MoveDown(n int) {
+	m.Cursor = clamp(m.Cursor+n, 0, len(m.Props().Visible())-1)
+	m.UpdateViewport()
+
+	switch {
+	case m.end == len(m.Props().Visible()):
+		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset-n, 1, m.Viewport.Height))
+	case m.Cursor > (m.end-m.start)/2:
+		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset-n, 1, m.Cursor))
+	case m.Viewport.YOffset > 1:
+	case m.Cursor > m.Viewport.YOffset+m.Viewport.Height-1:
+		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset+1, 0, 1))
+	}
+}
+
+// UpdateViewport updates the list content based on the previously defined
+// columns and rows.
+func (m *List) UpdateViewport() {
+	renderedRows := make([]string, 0, len(m.Props().Visible()))
+
+	// Render only rows from: m.cursor-m.viewport.Height to: m.cursor+m.viewport.Height
+	// Constant runtime, independent of number of rows in a table.
+	// Limits the number of renderedRows to a maximum of 2*m.viewport.Height
+	if m.Cursor >= 0 {
+		m.start = clamp(m.Cursor-m.Viewport.Height, 0, m.Cursor)
+	} else {
+		m.start = 0
+	}
+	m.end = clamp(m.Cursor+m.Viewport.Height, m.Cursor, len(m.Props().Visible()))
+	for i := m.start; i < m.end; i++ {
+		renderedRows = append(renderedRows, strconv.Itoa(i))
+	}
+
+	m.Viewport.SetContent(
+		lipgloss.JoinVertical(lipgloss.Left, renderedRows...),
+	)
+}
+
 func (m *List) View() string {
 	start, end := m.Paginator.GetSliceBounds(len(m.Props().Visible()))
 
@@ -157,3 +217,23 @@ func (m *List) View() string {
 }
 
 func (m List) Init() tea.Cmd { return nil }
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+func clamp(v, low, high int) int {
+	return min(max(v, low), high)
+}
