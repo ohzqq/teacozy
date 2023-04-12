0f8044792e759e73271af6c492f7cd786e5d8f97
ohzqq <iamchurkey@gmail.com>
2023-04-12 18:13:22 -0400

    currently works

--- app/app.go
+++ app/app.go
@@ -1,366 +1,120 @@
 package app
 
 import (
-	"strings"
-
-	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 	"github.com/londek/reactea"
-	"github.com/ohzqq/teacozy/keys"
+	"github.com/londek/reactea/router"
 	"github.com/ohzqq/teacozy/message"
 	"github.com/ohzqq/teacozy/style"
 	"github.com/ohzqq/teacozy/util"
 )
 
-type Option func(*List)
-
-type List struct {
-	reactea.BasicComponent // It implements AfterUpdate() for us, so we don't have to care!
+type App struct {
+	reactea.BasicComponent
 	reactea.BasicPropfulComponent[reactea.NoProps]
 
-	choices     []map[string]string
-	focus       bool
-	quitting    bool
-	Selected    map[int]struct{}
-	Cursor      int
-	height      int
-	width       int
-	NumSelected int
-	Limit       int
-	Style       style.List
-	KeyMap      keys.KeyMap
-	props       *Props
-	matches     []string
+	mainRouter reactea.Component[router.Props]
 
-	Viewport viewport.Model
-	start    int
-	end      int
+	Style     style.App
+	width     int
+	PrevRoute string
+	height    int
+	Choices   []map[string]string
 }
 
-type Choices []map[string]string
-
-func (i Choices) String(idx int) string {
-	var str string
-	item := i[idx]
-	for _, v := range item {
-		str = v
+func New(choices []string) *App {
+	return &App{
+		mainRouter: router.New(),
+		width:      util.TermHeight(),
+		height:     util.TermWidth(),
+		Choices:    MapChoices(choices),
+		Style:      style.DefaultAppStyle(),
 	}
-	return str
 }
 
-func (i Choices) Len() int {
-	return len(i)
-}
+func (c *App) Init(reactea.NoProps) tea.Cmd {
+	// Does it remind you of something? react-router!
+	return c.mainRouter.Init(map[string]router.RouteInitializer{
+		"default": func(router.Params) (reactea.SomeComponent, tea.Cmd) {
+			component := NewList()
 
-type Props struct {
-	Items   []Item
-	Matches []Item
-}
-
-func New(props []string, opts ...Option) *List {
-	m := List{
-		focus:  true,
-		width:  util.TermWidth(),
-		height: util.TermHeight(),
-		Style:  style.ListDefaults(),
-		props: &Props{
-			Items:   ChoicesToMatch(props),
-			Matches: ChoicesToMatch(props),
+			return component, component.Init(Props{
+				Choices: c.Choices, // Can also use "lambdas" (function can be created here)
+			})
 		},
-		choices: MapChoices(props),
-	}
-
-	for _, opt := range opts {
-		opt(&m)
-	}
-
-	m.Viewport = viewport.New(m.width, m.height)
-
-	m.KeyMap = m.DefaultKeyMap()
-	m.UpdateItems()
-
-	return &m
+	})
 }
 
-//func (m *List) UpdateProps(props *props.Items) {
-//  m.props = props
-//}
-
-func (m *List) Props() *Props {
-	return m.props
-}
-
-//func (m *List) Init() tea.Cmd {
-//  return nil
-//}
-
-func (c *List) Init(reactea.NoProps) tea.Cmd {
-	//reactea.SetCurrentRoute("list")
-	return nil
-}
-
-func (m List) DefaultKeyMap() keys.KeyMap {
-	var km = keys.KeyMap{
-		keys.Quit(),
-		keys.ToggleItem(),
-		keys.Up().WithKeys("up"),
-		keys.Down().WithKeys("down"),
-		keys.NewBinding("ctrl+u").
-			WithHelp("½ page up").
-			Cmd(message.Up(m.Viewport.Height / 2)),
-		keys.NewBinding("ctrl+d").
-			WithHelp("½ page down").
-			Cmd(message.Down(m.Viewport.Height / 2)),
-		keys.NewBinding("pgup").
-			WithHelp("page up").
-			Cmd(message.Up(m.Viewport.Height)),
-		keys.NewBinding("pgdown").
-			WithHelp("page down").
-			Cmd(message.Down(m.Viewport.Height)),
-		keys.NewBinding("end").
-			WithHelp("list bottom").
-			Cmd(message.Bottom()),
-		keys.NewBinding("home").
-			WithHelp("list top").
-			Cmd(message.Top()),
-	}
-	return km
-}
-
-func (m *List) Update(msg tea.Msg) tea.Cmd {
+func (c *App) Update(msg tea.Msg) tea.Cmd {
 	var cmds []tea.Cmd
-
 	switch msg := msg.(type) {
+	case message.ConfirmMsg:
+		//c.ConfirmAction = ""
+	case message.GetConfirmationMsg:
+		//c.ConfirmAction = msg.Question
+	case message.ChangeRouteMsg:
+		route := msg.Name
+		switch route {
+		case "filter":
+			//c.Footer("")
+		case "prev":
+			route = c.PrevRoute
+		case "help":
+			//p := c.NewProps(KeymapToProps(c.help))
+			//p.Height = c.Items.Height
+			//p.Width = c.Items.Width
+			//c.Routes["help"] = view.New().Initializer(p)
+		}
+		c.ChangeRoute(route)
+	case message.ReturnSelectionsMsg:
+		switch reactea.CurrentRoute() {
+		case "filter":
+			//if c.HasRoute("choose") {
+			//c.ChangeRoute("choose")
+			//}
+		default:
+			return reactea.Destroy
+		}
 	case message.QuitMsg:
-		cmds = append(cmds, tea.Quit)
-
-	case message.DownMsg:
-		m.MoveDown(msg.Lines)
-	case message.UpMsg:
-		m.MoveUp(msg.Lines)
-	case message.TopMsg:
-		m.GotoTop()
-	case message.BottomMsg:
-		m.GotoBottom()
+		return reactea.Destroy
 	case tea.KeyMsg:
-		if m.Focused() {
-			for _, k := range m.KeyMap {
-				if key.Matches(msg, k.Binding) {
-					cmds = append(cmds, k.TeaCmd)
-				}
-			}
+		switch msg.String() {
+		case "ctrl+c":
+			return reactea.Destroy
+		case "y":
+			cmds = append(cmds, message.Confirm(true))
+		case "n":
+			cmds = append(cmds, message.Confirm(false))
 		}
 	}
 
+	cmds = append(cmds, c.mainRouter.Update(msg))
 	return tea.Batch(cmds...)
 }
 
-func (m List) View() string {
-	return m.Viewport.View()
+func (c *App) Render(width, height int) string {
+	view := c.mainRouter.Render(width, height)
+
+	//if c.header != "" {
+	//view = lipgloss.JoinVertical(lipgloss.Left, c.header, view)
+	//}
+
+	//if c.ConfirmAction != "" {
+	//c.Footer(fmt.Sprintf("%s\n", c.Style.Confirm.Render(c.ConfirmAction+"(y/n)")))
+	//}
+
+	//if c.footer != "" {
+	//view = lipgloss.JoinVertical(lipgloss.Left, view, c.footer)
+	//}
+
+	return view
 }
 
-func (m *List) Render(w, h int) string {
-	m.SetWidth(m.width)
-	m.SetHeight(m.height)
-	m.UpdateItems()
-	m.Viewport.SetContent(
-		lipgloss.JoinVertical(lipgloss.Left, m.matches...),
-	)
-	return m.View()
-}
-
-// UpdateItems updates the list content based on the previously defined
-// columns and rows.
-func (m *List) UpdateItems() {
-	renderedRows := make([]string, 0, len(m.Props().Matches))
-
-	// Render only rows from: m.cursor-m.viewport.Height to: m.cursor+m.viewport.Height
-	// Constant runtime, independent of number of rows in a table.
-	// Limits the number of renderedRows to a maximum of 2*m.viewport.Height
-	if m.Cursor >= 0 {
-		m.start = clamp(m.Cursor-m.Viewport.Height, 0, m.Cursor)
-	} else {
-		m.start = 0
-		m.SetCursor(0)
-	}
-	m.end = clamp(m.Cursor+m.Viewport.Height, m.Cursor, len(m.Props().Matches))
-
-	if m.Cursor > m.end {
-		m.SetCursor(clamp(m.Cursor+m.Viewport.Height, m.Cursor, len(m.Props().Matches)-1))
-	}
-
-	for i := m.start; i < m.end; i++ {
-		renderedRows = append(renderedRows, m.renderRow(i))
-	}
-	m.SetMatches(renderedRows)
-
-}
-
-func (m *List) renderRow(rowID int) string {
-	row := m.Props().Matches[rowID]
-
-	var s strings.Builder
-
-	var cur bool
-	switch {
-	case rowID == m.Cursor:
-		cur = true
-	}
-
-	s.WriteString(row.Render(cur, m.Viewport.Width, m.Viewport.Height))
-
-	return s.String()
-}
-
-func (m *List) SetMatches(matches []string) {
-	m.matches = matches
-}
-
-// SelectedRow returns the selected row.
-// You can cast it to your own implementation.
-func (m List) CurrentItem() Item {
-	return m.Props().Matches[m.Cursor]
-}
-
-// MoveUp moves the selection up by any number of rows.
-// It can not go above the first row.
-func (m *List) MoveUp(n int) {
-	m.SetCursor(clamp(m.Cursor-n, 0, len(m.Props().Matches)-1))
-	m.UpdateItems()
-	switch {
-	case m.start == 0:
-		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset, 0, m.Cursor))
-	case m.start < m.Viewport.Height:
-		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset+n, 0, m.Cursor))
-	case m.Viewport.YOffset >= 1:
-		m.Viewport.YOffset = clamp(m.Viewport.YOffset+n, 1, m.Viewport.Height)
-	}
-}
-
-// MoveDown moves the selection down by any number of rows.
-// It can not go below the last row.
-func (m *List) MoveDown(n int) {
-	m.SetCursor(clamp(m.Cursor+n, 0, len(m.Props().Matches)-1))
-	m.UpdateItems()
-	switch {
-	case m.end == len(m.Props().Matches):
-		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset-n, 1, m.Viewport.Height))
-	case m.Cursor > (m.end-m.start)/2:
-		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset-n, 1, m.Cursor))
-	case m.Viewport.YOffset > 1:
-	case m.Cursor > m.Viewport.YOffset+m.Viewport.Height-1:
-		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset+1, 0, 1))
-	}
-}
-
-// GotoTop moves the selection to the first row.
-func (m *List) GotoTop() {
-	m.MoveUp(m.Cursor)
-}
-
-// GotoBottom moves the selection to the last row.
-func (m *List) GotoBottom() {
-	m.MoveDown(len(m.Props().Matches))
-}
-
-//func (m *List) ToggleAllItems() tea.Cmd {
-//  return func() tea.Msg {
-//    var items []int
-//    for _, item := range m.Props().AllItems() {
-//      items = append(items, item.Index)
-//    }
-//    m.Props().ToggleSelection(items...)
-//    return nil
-//  }
-//}
-
-func (m *List) quit() tea.Cmd {
-	m.quitting = true
-	return message.Quit()
-}
-
-// Focused returns the focus state of the table.
-func (m List) Focused() bool {
-	return m.focus
-}
-
-// Focus focuses the table, allowing the user to move around the rows and
-// interact.
-func (m *List) Focus() {
-	m.focus = true
-	m.Props().Matches = m.Props().Items
-	m.UpdateItems()
-}
-
-// Blur blurs the table, preventing selection or movement.
-func (m *List) Blur() {
-	m.focus = false
-	m.UpdateItems()
-}
-
-// VisibleItems returns the current rows.
-func (m List) VisibleItems() []Item {
-	return m.Props().Matches
-}
-
-// SetItems sets a new rows state.
-func (m *List) SetItems(r []Item) {
-	m.Props().Matches = r
-	m.UpdateItems()
-}
-
-// SetWidth sets the width of the viewport of the table.
-func (m *List) SetWidth(w int) {
-	m.Viewport.Width = w
-	m.UpdateItems()
-}
-
-// SetHeight sets the height of the viewport of the table.
-func (m *List) SetHeight(h int) {
-	m.Viewport.Height = h
-	m.UpdateItems()
-}
-
-// Height returns the viewport height of the table.
-func (m List) Height() int {
-	return m.Viewport.Height
-}
-
-// Width returns the viewport width of the table.
-func (m List) Width() int {
-	return m.Viewport.Width
-}
-
-// Cursor returns the index of the selected row.
-func (m List) GetCursor() int {
-	return m.Cursor
-}
-
-// SetCursor sets the cursor position in the table.
-func (m *List) SetCursor(n int) {
-	//m.Props().SetCursor(clamp(n, 0, len(m.Props().Matches)-1))
-	m.Cursor = clamp(n, 0, len(m.Props().Matches)-1)
-	m.UpdateItems()
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-
-	return b
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-
-	return b
-}
-
-func clamp(v, low, high int) int {
-	return min(max(v, low), high)
+func (c *App) ChangeRoute(r string) {
+	//if p := reactea.CurrentRoute(); p == "" {
+	//c.PrevRoute = "default"
+	//} else {
+	//c.PrevRoute = p
+	//}
+	reactea.SetCurrentRoute(r)
 }
---
+++ app/list.go
@@ -0,0 +1,349 @@
+package app
+
+import (
+	"strings"
+
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/londek/reactea"
+	"github.com/ohzqq/teacozy/keys"
+	"github.com/ohzqq/teacozy/message"
+	"github.com/ohzqq/teacozy/style"
+)
+
+type Option func(*List)
+
+type List struct {
+	reactea.BasicComponent // It implements AfterUpdate() for us, so we don't have to care!
+	reactea.BasicPropfulComponent[Props]
+
+	choices     []map[string]string
+	focus       bool
+	quitting    bool
+	Selected    map[int]struct{}
+	Cursor      int
+	height      int
+	width       int
+	NumSelected int
+	Limit       int
+	Style       style.List
+	KeyMap      keys.KeyMap
+	props       *Props
+
+	Viewport viewport.Model
+	start    int
+	end      int
+}
+
+type Choices []map[string]string
+
+func (i Choices) String(idx int) string {
+	var str string
+	item := i[idx]
+	for _, v := range item {
+		str = v
+	}
+	return str
+}
+
+func (i Choices) Len() int {
+	return len(i)
+}
+
+type Props struct {
+	Choices []map[string]string
+	Items   []Item
+	Matches []Item
+}
+
+func NewList(opts ...Option) *List {
+	m := List{
+		focus: true,
+		Style: style.ListDefaults(),
+	}
+
+	for _, opt := range opts {
+		opt(&m)
+	}
+
+	return &m
+}
+
+//func (m *List) UpdateProps(props *props.Items) {
+//  m.props = props
+//}
+
+func (m *List) Init(props Props) tea.Cmd {
+	props.Items = ChoiceMapToMatch(props.Choices)
+	props.Matches = props.Items
+	m.UpdateProps(props)
+
+	m.Viewport = viewport.New(0, 0)
+
+	m.KeyMap = m.DefaultKeyMap()
+	m.UpdateItems()
+
+	return nil
+}
+
+func (m List) DefaultKeyMap() keys.KeyMap {
+	var km = keys.KeyMap{
+		keys.Quit(),
+		keys.ToggleItem(),
+		keys.Up().WithKeys("up"),
+		keys.Down().WithKeys("down"),
+		keys.NewBinding("ctrl+u").
+			WithHelp("½ page up").
+			Cmd(message.Up(m.Viewport.Height / 2)),
+		keys.NewBinding("ctrl+d").
+			WithHelp("½ page down").
+			Cmd(message.Down(m.Viewport.Height / 2)),
+		keys.NewBinding("pgup").
+			WithHelp("page up").
+			Cmd(message.Up(m.Viewport.Height)),
+		keys.NewBinding("pgdown").
+			WithHelp("page down").
+			Cmd(message.Down(m.Viewport.Height)),
+		keys.NewBinding("end").
+			WithHelp("list bottom").
+			Cmd(message.Bottom()),
+		keys.NewBinding("home").
+			WithHelp("list top").
+			Cmd(message.Top()),
+	}
+	return km
+}
+
+func (m *List) Update(msg tea.Msg) tea.Cmd {
+	var cmds []tea.Cmd
+
+	switch msg := msg.(type) {
+	case message.QuitMsg:
+		cmds = append(cmds, tea.Quit)
+
+	case message.DownMsg:
+		m.MoveDown(msg.Lines)
+	case message.UpMsg:
+		m.MoveUp(msg.Lines)
+	case message.TopMsg:
+		m.GotoTop()
+	case message.BottomMsg:
+		m.GotoBottom()
+	case tea.KeyMsg:
+		if m.Focused() {
+			for _, k := range m.KeyMap {
+				if key.Matches(msg, k.Binding) {
+					cmds = append(cmds, k.TeaCmd)
+				}
+			}
+		}
+	}
+
+	return tea.Batch(cmds...)
+}
+
+func (m List) View() string {
+	return m.Viewport.View()
+}
+
+func (m *List) Render(w, h int) string {
+	m.SetWidth(w)
+	m.SetHeight(h)
+	m.UpdateItems()
+	return m.View()
+}
+
+// UpdateItems updates the list content based on the previously defined
+// columns and rows.
+func (m *List) UpdateItems() {
+	renderedRows := make([]string, 0, len(m.Props().Matches))
+
+	// Render only rows from: m.cursor-m.viewport.Height to: m.cursor+m.viewport.Height
+	// Constant runtime, independent of number of rows in a table.
+	// Limits the number of renderedRows to a maximum of 2*m.viewport.Height
+	if m.Cursor >= 0 {
+		m.start = clamp(m.Cursor-m.Viewport.Height, 0, m.Cursor)
+	} else {
+		m.start = 0
+		m.SetCursor(0)
+	}
+	m.end = clamp(m.Cursor+m.Viewport.Height, m.Cursor, len(m.Props().Matches))
+
+	if m.Cursor > m.end {
+		m.SetCursor(clamp(m.Cursor+m.Viewport.Height, m.Cursor, len(m.Props().Matches)-1))
+	}
+
+	for i := m.start; i < m.end; i++ {
+		renderedRows = append(renderedRows, m.renderRow(i))
+	}
+
+	m.Viewport.SetContent(
+		lipgloss.JoinVertical(lipgloss.Left, renderedRows...),
+	)
+
+}
+
+func (m *List) renderRow(rowID int) string {
+	row := m.Props().Matches[rowID]
+
+	var s strings.Builder
+
+	var cur bool
+	switch {
+	case rowID == m.Cursor:
+		cur = true
+	}
+
+	s.WriteString(row.Render(cur, m.Viewport.Width, m.Viewport.Height))
+
+	return s.String()
+}
+
+// SelectedRow returns the selected row.
+// You can cast it to your own implementation.
+func (m List) CurrentItem() Item {
+	return m.Props().Matches[m.Cursor]
+}
+
+// MoveUp moves the selection up by any number of rows.
+// It can not go above the first row.
+func (m *List) MoveUp(n int) {
+	m.SetCursor(clamp(m.Cursor-n, 0, len(m.Props().Matches)-1))
+	m.UpdateItems()
+	switch {
+	case m.start == 0:
+		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset, 0, m.Cursor))
+	case m.start < m.Viewport.Height:
+		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset+n, 0, m.Cursor))
+	case m.Viewport.YOffset >= 1:
+		m.Viewport.YOffset = clamp(m.Viewport.YOffset+n, 1, m.Viewport.Height)
+	}
+}
+
+// MoveDown moves the selection down by any number of rows.
+// It can not go below the last row.
+func (m *List) MoveDown(n int) {
+	m.SetCursor(clamp(m.Cursor+n, 0, len(m.Props().Matches)-1))
+	m.UpdateItems()
+	switch {
+	case m.end == len(m.Props().Matches):
+		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset-n, 1, m.Viewport.Height))
+	case m.Cursor > (m.end-m.start)/2:
+		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset-n, 1, m.Cursor))
+	case m.Viewport.YOffset > 1:
+	case m.Cursor > m.Viewport.YOffset+m.Viewport.Height-1:
+		m.Viewport.SetYOffset(clamp(m.Viewport.YOffset+1, 0, 1))
+	}
+}
+
+// GotoTop moves the selection to the first row.
+func (m *List) GotoTop() {
+	m.MoveUp(m.Cursor)
+}
+
+// GotoBottom moves the selection to the last row.
+func (m *List) GotoBottom() {
+	m.MoveDown(len(m.Props().Matches))
+}
+
+//func (m *List) ToggleAllItems() tea.Cmd {
+//  return func() tea.Msg {
+//    var items []int
+//    for _, item := range m.Props().AllItems() {
+//      items = append(items, item.Index)
+//    }
+//    m.Props().ToggleSelection(items...)
+//    return nil
+//  }
+//}
+
+func (m *List) quit() tea.Cmd {
+	m.quitting = true
+	return message.Quit()
+}
+
+// Focused returns the focus state of the table.
+func (m List) Focused() bool {
+	return m.focus
+}
+
+// Focus focuses the table, allowing the user to move around the rows and
+// interact.
+func (m *List) Focus() {
+	m.focus = true
+	//m.Props().Matches = m.Props().Items
+	m.UpdateItems()
+}
+
+// Blur blurs the table, preventing selection or movement.
+func (m *List) Blur() {
+	m.focus = false
+	m.UpdateItems()
+}
+
+// VisibleItems returns the current rows.
+func (m List) VisibleItems() []Item {
+	return m.Props().Matches
+}
+
+// SetItems sets a new rows state.
+func (m *List) SetItems(r []Item) {
+	//m.Props().Matches = r
+	m.UpdateItems()
+}
+
+// SetWidth sets the width of the viewport of the table.
+func (m *List) SetWidth(w int) {
+	m.Viewport.Width = w
+	m.UpdateItems()
+}
+
+// SetHeight sets the height of the viewport of the table.
+func (m *List) SetHeight(h int) {
+	m.Viewport.Height = h
+	m.UpdateItems()
+}
+
+// Height returns the viewport height of the table.
+func (m List) Height() int {
+	return m.Viewport.Height
+}
+
+// Width returns the viewport width of the table.
+func (m List) Width() int {
+	return m.Viewport.Width
+}
+
+// Cursor returns the index of the selected row.
+func (m List) GetCursor() int {
+	return m.Cursor
+}
+
+// SetCursor sets the cursor position in the table.
+func (m *List) SetCursor(n int) {
+	//m.Props().SetCursor(clamp(n, 0, len(m.Props().Matches)-1))
+	m.Cursor = clamp(n, 0, len(m.Props().Matches)-1)
+	//m.UpdateItems()
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+func clamp(v, low, high int) int {
+	return min(max(v, low), high)
+}
