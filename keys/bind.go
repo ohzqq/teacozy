8c821315159a1eb7e45fcf1b07c8f6516bd3c075
ohzqq <iamchurkey@gmail.com>
2023-04-22 15:17:15 -0400

    keys

---
+++ keys/bind.go
@@ -0,0 +1,118 @@
+package keys
+
+import "github.com/londek/reactea"
+
+func Enter() *Binding {
+	return New("enter")
+}
+
+func Filter() *Binding {
+	return New("/").
+		WithHelp("filter items").
+		Cmd(ChangeRoute("filter"))
+}
+
+func Save() *Binding {
+	return New("ctrl+s").
+		WithHelp("save edit")
+}
+
+func HalfPgUp() *Binding {
+	return New("ctrl+u").
+		WithHelp("½ page up").
+		Cmd(HalfPageUp)
+}
+
+func HalfPgDown() *Binding {
+	return New("ctrl+d").
+		WithHelp("½ page down").
+		Cmd(HalfPageDown)
+}
+
+func PgUp() *Binding {
+	return New("pgup").
+		WithHelp("page up").
+		Cmd(PageUp)
+}
+
+func PgDown() *Binding {
+	return New("pgdown").
+		WithHelp("page down").
+		Cmd(PageDown)
+}
+
+func End() *Binding {
+	return New("end").
+		WithHelp("list bottom").
+		Cmd(Bottom)
+}
+
+func Home() *Binding {
+	return New("home").
+		WithHelp("list top").
+		Cmd(Top)
+}
+
+func Up() *Binding {
+	return New("up").
+		WithHelp("move up").
+		Cmd(LineUp)
+}
+
+func Down() *Binding {
+	return New("down").
+		WithHelp("move down").
+		Cmd(LineDown)
+}
+
+func Next() *Binding {
+	return New("right").
+		WithHelp("next page").
+		Cmd(NextPage)
+}
+
+func Prev() *Binding {
+	return New("left").
+		WithHelp("prev page").
+		Cmd(PrevPage)
+}
+
+func Toggle() *Binding {
+	return New("tab").
+		WithHelp("select item").
+		Cmd(ToggleItem)
+}
+
+func Quit() *Binding {
+	return New("ctrl+c").
+		WithHelp("quit program").
+		Cmd(reactea.Destroy)
+}
+
+func Help() *Binding {
+	return New("f1").
+		WithHelp("show help").
+		Cmd(ShowHelp)
+}
+
+func Yes() *Binding {
+	return New("y").
+		WithHelp("confirm action")
+}
+
+func No() *Binding {
+	return New("n").
+		WithHelp("reject action")
+}
+
+func Esc() *Binding {
+	return New("esc").
+		WithHelp("exit screen").
+		Cmd(ReturnToList)
+}
+
+func Edit() *Binding {
+	return New("e").
+		WithHelp("edit field").
+		Cmd(ChangeRoute("edit"))
+}
