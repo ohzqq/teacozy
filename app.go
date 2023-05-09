26f9b8a4ed866cc67b3d90059029eba348c2fc29
ohzqq <iamchurkey@gmail.com>
2023-05-09 11:46:03 -0400

    ok

---
+++ app.go
@@ -0,0 +1,61 @@
+package teacozy
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/londek/reactea"
+)
+
+type App struct {
+	reactea.BasicComponent
+	reactea.BasicPropfulComponent[reactea.NoProps]
+
+	Routes map[PlaceHolder]PageComponent
+	page   *Page
+}
+
+func New(routes map[PlaceHolder]PageComponent) *App {
+	return &App{
+		Routes: routes,
+	}
+}
+
+func (c *App) Init(reactea.NoProps) tea.Cmd {
+	return c.InitializePage()
+}
+
+func (c *App) InitializePage() tea.Cmd {
+	for ph, page := range c.Routes {
+		if params, ok := ph.Matches(reactea.CurrentRoute()); ok {
+			p, cmd := page.Initialize(params)
+			c.page = p
+			return cmd
+		}
+	}
+	return nil
+}
+
+func (c *App) Update(msg tea.Msg) tea.Cmd {
+	reactea.AfterUpdate(c)
+
+	var cmd tea.Cmd
+	var cmds []tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if msg.String() == "ctrl+c" {
+			return reactea.Destroy
+		}
+	}
+
+	c.page, cmd = c.page.Update(msg)
+	cmds = append(cmds, cmd)
+
+	return tea.Batch(cmds...)
+}
+
+func (c *App) Render(w, h int) string {
+	if c.page == nil {
+		return "404 not found"
+	}
+	return c.page.View()
+}
--- types.go
+++ types.go
@@ -1,43 +1,136 @@
 package teacozy
 
 import (
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/londek/reactea"
 )
 
-type Page interface {
+type PlaceHolder string
+
+func (ph PlaceHolder) Matches(route string) (map[string]string, bool) {
+	return reactea.RouteMatchesPlaceholder(route, string(ph))
+}
+
+type PageComponent interface {
 	Header() reactea.SomeComponent
 	Main() reactea.SomeComponent
 	Footer() reactea.SomeComponent
+	Initialize(map[string]string) (*Page, tea.Cmd)
 }
 
-type BasicPage struct {
+type Page struct {
 	reactea.BasicComponent
-	header string
-	main   string
-	footer string
+	header reactea.SomeComponent
+	main   reactea.SomeComponent
+	footer reactea.SomeComponent
+	width  int
+	height int
 }
 
-func NewPage() *BasicPage {
-	return &BasicPage{
-		header: "header",
-		main:   "main",
-		footer: "footer",
+type PageProps struct {
+	Width  int
+	Height int
+}
+
+func NewPage(w, h int) *Page {
+	return &Page{
+		width:  w,
+		height: h,
 	}
 }
 
-func (c *BasicPage) Header() reactea.BasicComponent {
-	return reactea.Componentify[string](c.Render)
+func (c *Page) Initialize(params map[string]string) (*Page, tea.Cmd) {
+	return c, nil
 }
 
-func (c *BasicPage) Main() reactea.BasicComponent {
-	return reactea.Componentify[string](c.Render)
+func (c *Page) Header() reactea.SomeComponent {
+	return c.header
 }
 
-func (c *BasicPage) Footer() reactea.BasicComponent {
-	return reactea.Componentify[string](c.Render)
+func (c *Page) Main() reactea.SomeComponent {
+	return c.main
 }
 
-func (c *BasicPage) Render(w, h int) string {
-	return lipgloss.JoinVertical(lipgloss.Left, c.header, c.main, c.footer)
+func (c *Page) Footer() reactea.SomeComponent {
+	return c.footer
+}
+
+func (c *Page) SetHeader(comp reactea.SomeComponent) {
+	c.header = comp
+}
+
+func (c *Page) SetMain(comp reactea.SomeComponent) {
+	c.main = comp
+}
+
+func (c *Page) SetFooter(comp reactea.SomeComponent) {
+	c.footer = comp
+}
+
+func (c *Page) Update(msg tea.Msg) (*Page, tea.Cmd) {
+	reactea.AfterUpdate(c)
+
+	//var cmd tea.Cmd
+	var cmds []tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if msg.String() == "ctrl+c" {
+			return c, reactea.Destroy
+		}
+	}
+
+	if c.header != nil {
+		cmds = append(cmds, c.header.Update(msg))
+	}
+
+	if c.main != nil {
+		cmds = append(cmds, c.main.Update(msg))
+	}
+
+	if c.footer != nil {
+		cmds = append(cmds, c.footer.Update(msg))
+	}
+
+	return c, tea.Batch(cmds...)
+}
+
+func (c *Page) View() string {
+	return c.Render(c.width, c.height)
+}
+
+func (c *Page) Render(w, h int) string {
+	var views []string
+
+	if c.header != nil {
+		if head := c.header.Render(w, h); head != "" {
+			views = append(views, head)
+			h -= lipgloss.Height(head)
+		}
+	}
+
+	var footer string
+	if c.footer != nil {
+		if f := c.footer.Render(w, h); f != "" {
+			footer = f
+			h -= lipgloss.Height(footer)
+		}
+	}
+
+	if c.main != nil {
+		if m := c.main.Render(w, h); m != "" {
+			views = append(views, m)
+		}
+	}
+
+	if footer != "" {
+		views = append(views, footer)
+	}
+
+	return lipgloss.JoinVertical(lipgloss.Left, views...)
+}
+
+func (c Page) Init() tea.Cmd {
+	return nil
 }
